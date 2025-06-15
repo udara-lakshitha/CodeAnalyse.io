5a8fe911bd798827fb97030b30475bb9ee4a0eea
udara-lakshitha <lakshithau95@gmail.com>
2025-06-15 23:35:11 +0530

    feat(java): configure database and create user entity/repository

---
+++ backend-java/src/main/java/io/codeanalyse/server/model/User.java
@@ -0,0 +1,23 @@
+package io.codeanalyse.server.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+}
---
+++ backend-java/src/main/java/io/codeanalyse/server/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package io.codeanalyse.server.repository;
+
+
+import io.codeanalyse.server.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
